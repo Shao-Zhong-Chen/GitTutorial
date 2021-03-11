944c169b7872934fb101a2eb593464854b1c60f2
陳柏錩 <t108590033@ntut.org.tw>
2021-03-12 00:37:52 +0800

    practice8

---
+++ GitTutorial/GitTutorial.cpp
@@ -0,0 +1,30 @@
+﻿#include <string>
+#include <iostream>
+#include <cstlib>
+#include <ctime>
+bool Guess(int number) {
+	static int target = -1;
+
+	srand(time(NULL));
+	if (target == -1) {
+		target = rand() % 100 + 1;
+	}
+	if (number == target) {
+		std::cout << "Correct !!";
+		target = -1;
+		return true;
+	}
+	else {
+		std::cout << "Wrong" << std::endl;
+		return false;
+	}
+}
+
+int main() {
+
+	int guess;
+	do {
+		std::cin >> guess;
+	} while (!Guess(guess));
+	return 0;
+}
